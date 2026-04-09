9e2167045447f5edeb87d8b5d43226c8e3e23f23
chrisbunny <christopherjbunny@gmail.com>
2026-04-09 15:28:03 -0500

    Finished Step #1

--- src/main/java/com/pluralsight/BasicCalculator.java
+++ src/main/java/com/pluralsight/BasicCalculator.java
@@ -1,4 +1,36 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 public class BasicCalculator {
+    public static void main(String[] args) {
+
+        //Step #1
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+
+        int firstNumber = myScanner.nextInt();
+
+        System.out.println("Enter the second number: ");
+
+        int secondNumber = myScanner.nextInt();
+
+        System.out.println("Possible calculations: ");
+        System.out.println("    (A)Add");
+        System.out.println("    (S)Subtract");
+        System.out.println("    (M)Multiply");
+        System.out.println("    (D)Divide");
+        System.out.println("Please select an option: ");
+        myScanner.nextLine();
+
+        String letterOption = myScanner.nextLine();
+
+        System.out.println(firstNumber + " * " + secondNumber + " = " + firstNumber * secondNumber);
+
+
+
+
+
+
+    }
 }
